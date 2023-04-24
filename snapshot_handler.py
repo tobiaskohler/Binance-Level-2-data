9c5102812d24768a399bcd14f18215f3068f5171
tobiaskohler <tobias@kohler.com>
2023-04-24 14:07:35 +0200

    working snapshot implementation

--- snapshot_handler.py
+++ snapshot_handler.py
@@ -1,22 +1,67 @@
+import asyncio
+import aiofiles
+import time
+import httpx
+import ujson
+from misc import load_config
+from directory_handler import check_directory_structure
 
+date = time.strftime("%Y%m%d")
 
-
-
-rest_url = f'https://api.binance.com/api/v3/depth'
+async def get_snapshot(pair, data_warehouse_path, orderbook_depth):
     
-rest_params = {
-    "symbol": pair.upper(),
-    "limit": 5000,
-}
-
+    rest_url = f'https://api.binance.com/api/v3/depth'
     
-async with httpx.AsyncClient() as client:
-    snapshot = await client.get(rest_url, params=rest_params)
+    rest_params = {
+        "symbol": pair.upper(),
+        "limit": orderbook_depth,
+    }
+    
+    timestamp = time.strftime("%Y%m%d%H%M%S")
 
-snapshot = snapshot.ujson()
-snapshot['time'] = time.time()
+    async with httpx.AsyncClient() as client:
+        snapshot = await client.get(rest_url, params=rest_params)
+
+    async with aiofiles.open(f'{data_warehouse_path}/{date}/{pair}/snapshots/{timestamp}.txt', mode='w') as f:
+        await f.write(snapshot.text + '\n')
 
 
-async with aiofiles.open(f'{pair_lower}_updates_{timestamp}.txt', mode='w') as f:
-    await f.write(snapshot.text + '\n')
+# RUN THIS IN WHILE LOOP EVERY n-TH SECONDS
+async def get_snapshots_automatically(data_warehouse_path, symbols, orderbook_depth):
+    
+    tasks = []
+    
+    for pair in symbols:
+        tasks.append(get_snapshot(pair, data_warehouse_path, orderbook_depth))
+        
+    await asyncio.gather(*tasks)
+    
+async def take_snapshot_loop(snapshot_interval):
+    
+    while True:
+        await get_snapshots_automatically(data_warehouse_path, symbols, orderbook_depth)
+        await asyncio.sleep(snapshot_interval)
 
+
+
+async def get_snapshots_manually(data_warehouse_path, symbols, orderbook_depth):
+    tasks = []
+    
+    for pair in symbols:
+        tasks.append(get_snapshot(pair, data_warehouse_path, orderbook_depth))
+        
+    await asyncio.gather(*tasks)
+
+
+
+if __name__ == '__main__':
+        
+        config = load_config()
+        check_directory_structure(config['data_warehouse_path'], config['symbols'])
+    
+        data_warehouse_path = config['data_warehouse_path']
+        symbols = config['symbols']
+        orderbook_depth = config['orderbook_depth']
+        snapshot_interval = config['snapshot_interval']
+    
+        asyncio.run(take_snapshot_loop(snapshot_interval))
--- websocket_handler.py
+++ websocket_handler.py
@@ -1,5 +1,6 @@
 import asyncio
 from websockets import connect
+from websockets import ConnectionClosedError, InvalidStatusCode, InvalidHandshake
 import aiofiles
 import time
 from misc import load_config
@@ -10,6 +11,8 @@ import threading
 date = time.strftime("%Y%m%d")
 
 async def orderbook_download(pair, data_warehouse_path):
+    config = load_config()
+    websocket_timeout = config['websocket_timeout']
     
     pair_lower = pair.lower()
     wss_url = f'wss://stream.binance.com:9443/ws/{pair_lower}@depth@100ms'
@@ -17,19 +20,40 @@ async def orderbook_download(pair, data_warehouse_path):
     
     async with connect(wss_url) as websocket:
         while True:
-            data = await websocket.recv()
-            
-            async with aiofiles.open(f'{data_warehouse_path}/{date}/{pair}/updates/{timestamp}', mode='a') as f:
-                await f.write(data + '\n')
+            try:
+                data = await websocket.recv()
+                
+                async with aiofiles.open(f'{data_warehouse_path}/{date}/{pair}/updates/{timestamp}.txt', mode='a') as f:
+                    await f.write(data + '\n')
+                    
+            except ConnectionClosedError:
+                print('Connection closed, reconnecting...')
+                await asyncio.sleep(websocket_timeout)
+
+            except InvalidStatusCode as e:
+                print(f'Invalid status code: {e}')
+                await asyncio.sleep(websocket_timeout)
+
+            except InvalidHandshake as e:
+                print(f'Invalid handshake: {e}')
+                await asyncio.sleep(websocket_timeout)
+                
+            except Exception as e:
+                print(f'Unhandled exception: {e}')
+                break
     
 
 async def download_all_pairs(data_warehouse_path, symbols):
+    
     tasks = []
     for pair in symbols:
         tasks.append(orderbook_download(pair, data_warehouse_path))
+        
     await asyncio.gather(*tasks)
+    
 
 if __name__ == '__main__':
+    
     config = load_config()
     check_directory_structure(config['data_warehouse_path'], config['symbols'])
 
